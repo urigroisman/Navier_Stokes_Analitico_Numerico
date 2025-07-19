4d158ab36a739a009f78ba331c6c953ed4f69063
urigroisman <uri@fisica-newton.com>
2025-07-19 17:13:17 -0300

    Corrijo errores en capítulo 1 y agrego script de limpieza

---
+++ limpiar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Limpiando archivos auxiliares de LaTeX..."
+
+rm -f *.aux *.log *.out *.toc *.synctex.gz *.fls *.fdb_latexmk *.bbl *.blg *.lof *.lot
+
+echo "Listo. Carpeta limpia 🚮"
